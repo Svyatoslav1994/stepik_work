45d1bc39fd266dafdfd119cebc74a4f8b1781511
Svyatoslav1994 <82512062+Svyatoslav1994@users.noreply.github.com>
2021-11-01 18:16:08 +0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,7 @@
 sudo ﻿ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-gunicorn -b 0.0.0.0:8080 hello
+
+sudo ln -s /home/box/web/hello.py   /etc/gunicorn.d/hello.py
+
+sudo /etc/init.d/gunicorn restart
+$ gunicorn -b 0.0.0.0:8080 hello:app &
