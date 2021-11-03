3ff9cda4deafabceea268e2c284d18a23a392358
Svyatoslav1994 <82512062+Svyatoslav1994@users.noreply.github.com>
2021-11-03 18:40:28 +0400

    Update hello.py

--- etc/hello.py
+++ etc/hello.py
@@ -1,19 +1,11 @@
-#mode = 'wsgi'
-#working_dir = '/home/box/web'
-
-bind = "0.0.0.0:8080"
-#workers=16
-#timeout=60
-pythonpath="/home/box/web"
-
-#CONFIG = {                                                                      
-#    'mode': 'wsgi',                                                           
-#    'working_dir': '/home/box/web',                                           
-#    'python': '/usr/bin/python',                                              
-#    'args': (                                                                   
-#        '--bind=0.0.0.0:8080',                                              
-#        '--workers=16',                                                         
-#        '--timeout=60',                                                         
-#        'app.module',                                                           
-#    ),                                                                          
-#}    
+CONFIG = {
+    'mode': 'wsgi',
+    'working_dir': '/home/box/web/',
+    'python': '/usr/bin/python',
+    'args': (
+        '--bind=0.0.0.0:8080',
+        '--workers=3',
+        '--timeout=60',
+        'hello:wsgi_application',
+    ),
+}
