a85bca306276bd5f97a6393d040cb5f61344658a
Svyatoslav1994 <82512062+Svyatoslav1994@users.noreply.github.com>
2021-11-03 18:39:15 +0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,14 +1,4 @@
-
-sudo unlink /etc/nginx/sites-enabled/default
-sudo rm -rf /etc/nginx/sites-enabled/test.conf
-sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
-
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo /etc/init.d/gunicorn restart
 sudo /etc/init.d/nginx restart
-
-sudo rm -rf /etc/gunicorn.d/hello.py
-sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
-#sudo /etc/init.d/gunicorn restart 
-#sudo gunicorn -c /etc/gunicorn.d/hello.py hello:application
-gunicorn --bind='0.0.0.0:8080' hello:wsgi_application
-
-sudo /etc/init.d/mysql start
