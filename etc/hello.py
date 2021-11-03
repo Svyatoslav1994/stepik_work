c9616de365d0181d89f6abb7eb9fe1246e9c3287
Svyatoslav1994 <82512062+Svyatoslav1994@users.noreply.github.com>
2021-11-03 18:32:43 +0400

    Update hello.py

--- etc/hello.py
+++ etc/hello.py
@@ -1,19 +1,19 @@
 #mode = 'wsgi'
 #working_dir = '/home/box/web'
 
-#bind = "0.0.0.0:8080"
+bind = "0.0.0.0:8080"
 #workers=16
 #timeout=60
-#pythonpath="/home/box/web"
+pythonpath="/home/box/web"
 
-CONFIG = {                                                                      
-    'mode': 'wsgi',                                                           
-    'working_dir': '/home/box/web',                                           
-    'python': '/usr/bin/python',                                              
-    'args': (                                                                   
-        '--bind="0.0.0.0:8080"',                                              
-        '--workers=16',                                                         
-        '--timeout=60',                                                         
-        'hello:application',                                                           
-    ),                                                                          
-}    
+#CONFIG = {                                                                      
+#    'mode': 'wsgi',                                                           
+#    'working_dir': '/home/box/web',                                           
+#    'python': '/usr/bin/python',                                              
+#    'args': (                                                                   
+#        '--bind="0.0.0.0:8080"',                                              
+#        '--workers=16',                                                         
+#        '--timeout=60',                                                         
+#        'hello:application',                                                           
+#    ),                                                                          
+#}    
