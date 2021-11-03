0745ea79977dab401b0e2f401266a78fa4cbab80
Svyatoslav1994 <82512062+Svyatoslav1994@users.noreply.github.com>
2021-11-03 17:48:38 +0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -8,6 +8,6 @@ sudo rm -rf /etc/gunicorn.d/hello.py
 sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart 
 sudo gunicorn -c /etc/gunicorn.d/hello.py hello:application
-gunicorn --bind='0.0.0.0:8080' hello:wsgi_application
+gunicorn --bind='0.0.0.0:8080' hello:application
 
-sudo /etc/init.d/mysql start
+#sudo /etc/init.d/mysql start
